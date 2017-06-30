84b2e3c35ced3097337168e9b4690228063aea47
Amit Beka <--->
2017-06-30 13:51:18 +0300

    add skeleton of the functionality
    Signed-off-by: Amit Beka <--->

---
+++ conda-merge.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python3
+# encoding: utf-8
+"""Tool to merge environment files of the conda package manager"""
+
+def read_file(path):
+    pass
+
+
+def merge_name(*names):
+    pass
+
+
+def merge_channels(*channels_list):
+    pass
+
+
+def merge_dependencies(*deps_list):
+    pass
+
+
+def merge_pips(*pip_list):
+    pass
+
+
+def parse_args(argv=None):
+    pass
+
+
+def main(args):
+    pass
+
+
+if __name__ == '__main__':
+    main(parse_args())
