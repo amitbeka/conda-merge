325937fd9422b7958bdb96474f476d00c06baa6f
Amit Beka <--->
2017-07-01 08:57:54 +0300

    add merge_channels using DAG implementation
    Signed-off-by: Amit Beka <--->

--- conda_merge.py
+++ conda_merge.py
@@ -2,23 +2,15 @@
 # encoding: utf-8
 """Tool to merge environment files of the conda package manager"""
 
-def read_file(path):
+from collections import OrderedDict, deque
+from copy import deepcopy
+
+
+class MergeError(Exception):
     pass
 
 
-def merge_name(*names):
-    pass
-
-
-def merge_channels(*channels_list):
-    pass
-
-
-def merge_dependencies(*deps_list):
-    pass
-
-
-def merge_pips(*pip_list):
+def main(args):
     pass
 
 
@@ -26,9 +18,112 @@ def parse_args(argv=None):
     pass
 
 
-def main(args):
+def read_file(path):
     pass
 
+
+def merge_names(names):
+    """Merge names of environments by leaving the last non-blank one"""
+    actual_names = [name for name in names if name]
+    if actual_names:
+        return actual_names[-1]
+
+
+def merge_channels(channels_list):
+    dag = DAG()
+    try:
+        for channels in channels_list:
+            for i, channel in enumerate(channels):
+                dag.add_node(channel)
+                if i > 0:
+                    dag.add_edge(channels[i-1], channel)
+    except ValueError as exc:
+        raise MergeError("Can't satisfy priority {}".format(exc.msg))
+    return dag.topological_sort()
+
+
+def merge_dependencies(deps_list):
+    pass
+
+
+def merge_pips(pip_list):
+    pass
+
+
+class DAG(object):
+    """Directed acyclic graph fpr merging channel priorities.
+
+    This is a stripped down version adopted from:
+    https://github.com/thieman/py-dag
+
+    """
+
+    def __init__(self):
+        self.graph = OrderedDict()
+
+    def add_node(self, node_name):
+        if node_name not in self.graph:
+            self.graph[node_name] = set()
+
+    def add_edge(self, from_node, to_node):
+        if from_node not in self.graph or to_node not in self.graph:
+            raise KeyError('one or more nodes do not exist in graph')
+        test_graph = deepcopy(self.graph)
+        test_graph[from_node].add(to_node)
+        if self.validate():
+            self.graph[from_node].add(to_node)
+        else:
+            raise ValueError("{} -> {}".format(from_node, to_node))
+    @property
+    def independent_nodes(self):
+        """Return a list of all nodes in the graph with no dependencies."""
+        dependent_nodes = set(node for dependents in self.graph.values()
+                              for node in dependents)
+        return [node for node in self.graph.keys()
+                if node not in dependent_nodes]
+
+    def validate(self):
+        """Return whether the graph doesn't contain a cycle"""
+        if len(self.independent_nodes) > 0:
+            try:
+                self.topological_sort()
+                return True
+            except ValueError:
+                return False
+        return False
+
+    def topological_sort(self):
+        """Return a topological ordering of the DAG.
+
+        Raise an error if this is not possible (graph is not valid).
+
+        """
+        in_degree = {}
+        for node in self.graph:
+            in_degree[node] = 0
+
+        for from_node in self.graph:
+            for to_node in self.graph[from_node]:
+                in_degree[to_node] += 1
+
+        queue = deque()
+        for node in in_degree:
+            if in_degree[node] == 0:
+                queue.appendleft(node)
+
+        sorted_nodes = []
+        while queue:
+            independent_node = queue.pop()
+            sorted_nodes.append(independent_node)
+            for next_node in self.graph[independent_node]:
+                in_degree[next_node] -= 1
+                if in_degree[next_node] == 0:
+                    queue.appendleft(next_node)
+
+        if len(sorted_nodes) == len(self.graph):
+            return sorted_nodes
+        else:
+            raise ValueError('graph is not acyclic')
 
 if __name__ == '__main__':
     main(parse_args())
--- test_script.py
+++ test_script.py
@@ -3,7 +3,24 @@
 
 import pytest
 
-import conda_merge
+import conda_merge as cm
 
-def test_basic():
-    pass
+
+def test_merge_names():
+    """Should leave only the last name"""
+    names = ['a', None, 'b', 'c', None, '']
+    assert cm.merge_names(names) == 'c'
+    assert cm.merge_names([]) is None
+    assert cm.merge_names([None, None]) is None
+
+
+def test_channels():
+    # simple
+    assert cm.merge_channels([['a', 'b'], ['b', 'c']]) == ['a', 'b', 'c']
+    # contained
+    full = list('abcd')
+    assert cm.merge_channels([['a', 'b'], ['c', 'd'], full]) == full
+    # complex -- other options might be possible, so failing for no reason is possible
+    in_channels = [['a', 'b', 'c'], ['x', 'c', 'd'], ['b', 'f', 'd']]
+    possible_out = [list(x) for x in ('abfxcd', 'abxfcd', 'axbcfd')]
+    assert cm.merge_channels(in_channels) in possible_out
